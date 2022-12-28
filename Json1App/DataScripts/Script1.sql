283ab23003d8c6dabc2dc3fcf653742056f7b52b
Karen Payne <paynekaren@comcast.net>
2022-12-28 05:43:02 -0800

    added new select statement

--- Json1App/DataScripts/Script1.sql
+++ Json1App/DataScripts/Script1.sql
@@ -14,3 +14,14 @@ SELECT FirstName,
        JSON_VALUE(Addresses, '$[1].City')    AS City,
        JSON_VALUE(Addresses, '$[1].Company') AS Company
 FROM [EF.Json].dbo.Person;
+
+
+DECLARE @Index AS NCHAR(1) = 1;
+SELECT Id,
+		FirstName,
+		LastName,
+		DateOfBirth,
+		JSON_VALUE(Addresses, '$[' + @Index + '].Street') AS Street,
+		JSON_VALUE(Addresses, '$[' + @Index + '].City') AS City,
+		JSON_VALUE(Addresses, '$[' + @Index + '].Company') AS Company
+FROM dbo.Person;
